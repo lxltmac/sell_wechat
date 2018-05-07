b8e75c9c83a54d69bf7f60d6cb447b77c8cb8522
lxltmac <2449382248@qq.com>
2018-05-07 23:35:23 +0800

    修改冲突文件 websocket

---
+++ src/main/java/com/imooc/config/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.imooc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created by Administrator on 2018/5/7.
+ */
+@Component
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
